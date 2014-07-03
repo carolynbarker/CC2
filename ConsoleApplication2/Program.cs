47ed08a9c9e9af40ad5cb04f5fc5b6bf5ee4face
U-ZOO\csb <csb@CHINOOK.zoo.lan>
2014-07-03 11:59:16 +0100

    done, now let's bugfix

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -13,16 +13,30 @@
             PrintSomethingRelevant(tally);
         }
 
-        private static void PrintSomethingRelevant(tally )
+        private void PrintSomethingRelevant(int[] tally)
         {
             Console.Write("Enter desired frequency:");
             var freq = Console.ReadLine();
             FindThoseTLSs(freq, tally);
         }
 
-        private static void FindThoseTLSs(freq , tally )
+        private void FindThoseTLSs(int freq, int[] tally)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < tally.Length; i++)
+            {
+                if (tally[i] == freq)
+                {
+                    Deconvert(i);
+                }
+            }
+        }
+
+        private void Deconvert(int i)
+        {
+            var first = (char)((i % 27) + 97);
+            var second = (char)((((i - first) % (27 * 27)) / 27) + 97);
+            var third = (char)(((i - first - 27 * second) / (27 * 27)) + 97);
+            Console.WriteLine(first, second, third);
         }
     }
 }
--- ConsoleApplication2/TLSFinder.cs
+++ ConsoleApplication2/TLSFinder.cs
@@ -2,14 +2,17 @@
 {
     public class TLSFinder
     {
-        public string RegexString { get  return "[A-Za-z]{3}" }
+        public string RegexString
+        {
+            get { return "[A-Za-z]{3}"; }
+        }
 
         public int[] Count(string document)
         {
             string regex = RegexString();
-            var lowercase = document.ToLower() //to make the ascii conversions part easier
+            var lowercase = document.ToLower(); //to make the ascii conversions part easier
             var tally = FindAllTheThings.Find(lowercase, regex);
-            return tally
+            return tally;
         }
     }
 }
